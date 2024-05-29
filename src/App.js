02e01bef987bacf96c19a12914bfa5db7f202799
jbrace02 <57956271+jbrace02@users.noreply.github.com>
2024-05-28 23:12:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import "./App.css";
-import logo from "./logo.svg";
-import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
-import Tokenomics from "./Components/Tokenomics";
-import Buy from "./Components/Buy";
-import Footer from "./Components/Footer";
-
-// txt record = v=spf1 include:spf.efwd.registrar-servers.com ~all
+import React, { useState } from 'react';
+import './index.css';
+import logo from './logo.svg';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Tokenomics from './Components/Tokenomics';
+import Buy from './Components/Buy';
+import Footer from './Components/Footer';
 
 function App() {
-  return (
-    <>
-      <Navbar logo={logo} />
-      <Hero logo={logo} />
-      <Tokenomics />
-      <Buy />
-      <Footer logo={logo} />
-    </>
-  );
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setDarkMode(!darkMode);
+        if (darkMode) {
+            document.body.classList.remove('dark-mode');
+        } else {
+            document.body.classList.add('dark-mode');
+        }
+    };
+
+    return (
+        <>
+            <Navbar logo={logo} />
+            <button onClick={toggleDarkMode} style={{ position: 'fixed', top: '10px', right: '10px' }}>
+                Toggle Dark Mode
+            </button>
+            <Hero logo={logo} />
+            <Tokenomics />
+            <Buy />
+            <Footer logo={logo} />
+        </>
+    );
 }
 
 export default App;
+
