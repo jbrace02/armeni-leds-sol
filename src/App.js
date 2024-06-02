d3c05d6dd8c76f12dd82a7587ab2a29f2cd67de4
jbrace02 <57956271+jbrace02@users.noreply.github.com>
2024-06-02 16:13:03 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import logo from "./logo.svg";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Tokenomics from "./Components/Tokenomics";
+import Buy from "./Components/Buy";
+import Footer from "./Components/Footer";
+
+// txt record = v=spf1 include:spf.efwd.registrar-servers.com ~all
 
 function App() {
   return (
     <>
       <Navbar logo={logo} />
+      <Hero logo={logo} />
+      <Tokenomics />
+      <Buy />
+      <Footer logo={logo} />
     </>
   );
 }
