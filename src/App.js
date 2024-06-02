d8ddaa8ed036efa1620c3b9bb98f6b3584f9f3c2
jbrace02 <57956271+jbrace02@users.noreply.github.com>
2024-06-02 16:04:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import React, { useState } from 'react';
-import './index.css';
-import logo from './logo.svg';
-import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
-import Tokenomics from './Components/Tokenomics';
-import Buy from './Components/Buy';
-import Footer from './Components/Footer';
+import "./App.css";
+import logo from "./logo.svg";
+import Navbar from "./Components/Navbar";
 
 function App() {
-    const [darkMode, setDarkMode] = useState(false);
-
-    const toggleDarkMode = () => {
-        setDarkMode(prevMode => !prevMode); // This ensures state is correctly toggled based on the previous state
-        document.body.classList.toggle('dark-mode'); // Directly toggle the class without conditional
-    };
-
-    return (
-        <>
-            <Navbar logo={logo} />
-            <button 
-                onClick={toggleDarkMode} 
-                style={{ position: 'fixed', top: '10px', right: '10px' }}
-                className="toggle-button" // Added a class for potential CSS styling
-                aria-label={darkMode ? "Switch to light mode" : "Switch to dark mode"} // Accessibility label for the button
-            >
-                {darkMode ? 'Light Mode' : 'Dark Mode'} // Dynamic text based on the state
-            </button>
-            <Hero logo={logo} />
-            Tokenomics />
-            <Buy />
-            <Footer logo={logo} />
-        </>
-    );
+  return (
+    <>
+      <Navbar logo={logo} />
+    </>
+  );
 }
 
 export default App;
